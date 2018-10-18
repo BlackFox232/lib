5d9b2a3638f41d37df0cf797b761e92e49f8b214
Артем Симаков <artem.simakov.98@mail.ru>
2018-10-18 15:44:05 +0300

    v 3.0

--- BDKS-06/HighLevel.cs
+++ BDKS-06/HighLevel.cs
@@ -1,35 +1,218 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BDKS_06
+﻿namespace BDKS_06
 {
-    class HighLevel 
+    class HighLevel
     {
         public byte Adress { get; set; } = 0x1;
 
-        public byte[] Second(ushort b2 , ushort b4 )
+        public byte[] Second(ushort s2, ushort s4)
         {
-            byte[] val = LowLevel.Comm(Adress, 0x2, b2, b4);
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0x2,
+                s2,
+                s4);
 
             return val;
         }
 
-        public byte[] Third(ushort b2, ushort b4)
+        public byte[] Third(ushort s2, ushort s4)
         {
-            byte[] val = LowLevel.Comm(Adress, 0x2, b2, b4);
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0x3,
+                s2,
+                s4);
 
             return val;
         }
 
-        public byte[] Four(ushort b2, ushort b4)
+        public byte[] Four(ushort s2, ushort s4)
         {
-            byte[] val = LowLevel.Comm(Adress, 0x2, b2, b4);
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0x4,
+                s2,
+                s4);
 
             return val;
         }
-        
+
+        public byte[] Fived(ushort s2, ushort s4)
+        {
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0x5,
+                s2,
+                s4);
+
+            return val;
+        }
+
+        public byte[] Sixed(ushort s2, ushort s4)
+        {
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0x6,
+                s2,
+                s4);
+
+            return val;
+        }
+
+        public byte[] Seven()
+        {
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0x7);
+
+            return val;
+        }
+
+        public byte[] Eight(ushort s2, byte b3, byte b4)
+        {
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0x8,
+                s2,
+                b3,
+                b4);
+
+            return val;
+        }
+
+        public byte[] Nine(byte b2, ushort s2, ushort s4, ushort s6)
+        {
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0x9,
+                b2,
+                s2,
+                s4,
+                s6);
+
+            return val;
+        }
+
+        public byte[] Ten(byte b2, ushort s2, ushort s4, ushort s6, byte b7, byte b8, byte b9, byte b10, byte b11, byte b12)
+        {
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0xA,
+                b2,
+                s2,
+                s4,
+                s6,
+                b7,
+                b8,
+                b9,
+                b10,
+                b11,
+                b12);
+
+            return val;
+        }
+
+        public byte[] Eighteen(byte b2, ushort s2, ushort s4)
+        {
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0xB,
+                b2,
+                s2,
+                s4
+                );
+
+            return val;
+        }
+
+        public byte[] Sixteen(byte b2, ushort s2, ushort s4, ushort s6, ushort s8)
+        {
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0x10,
+                b2,
+                s2,
+                s4,
+                s6,
+                s8);
+
+            return val;
+        }
+
+        public byte[] Seventeen()
+        {
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0x11);
+
+            return val;
+        }
+
+        public byte[] Eightteen(byte b2, ushort s2, ushort s4)
+        {
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0x12,
+                b2,
+                s2,
+                s4);
+
+            return val;
+        }
+
+        public byte[] Nineteen(byte b2, ushort s2, ushort s4, byte b5, byte b6, byte b7, byte b8)
+        {
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0x13,
+                b2,
+                s2,
+                s4,
+                b5,
+                b6,
+                b7,
+                b8);
+
+            return val;
+        }
+
+        public byte[] ReadSpectreAndReboot()
+        {
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0x40);
+
+            return val;
+        }
+
+        public byte[] SzhatiiSpectreAndReboot()
+        {
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0x41);
+
+            return val;
+        }
+
+        public byte[] SpectreSzhatiiVid()
+        {
+            byte[] val = LowLevel.Comm(
+                Adress,
+                0x42);
+
+            return val;
+        }
+
+//public byte[] PrimerIskl(byte b2, ushort s2, ushort s4)
+//{
+//    byte[] val = LowLevel.Comm(
+//        Adress,
+//        0x4,
+//        b2,
+//        s2,
+//        s4);
+
+//    return val;
+//}
     }
 }
--- BDKS-06/LowLevel.cs
+++ BDKS-06/LowLevel.cs
@@ -2,7 +2,7 @@
 
 namespace BDKS_06
 {
-    public class LowLevel
+    public static class LowLevel
     {
         static Port port = new Port() ;
         static Crc16 crc = new Crc16();
@@ -38,9 +38,9 @@ namespace BDKS_06
             return msg;
         }
 
-        public static byte[] Comm(byte b1, byte b2, ushort b3, byte b4,byte b5 )
+        public static byte[] Comm(byte b1, byte b2, ushort s3, byte b4,byte b5 )
         {
-            byte[] bytesOne = BitConverter.GetBytes(b3);
+            byte[] bytesOne = BitConverter.GetBytes(s3);
 
             byte[] msg = new byte[] {
                 b1,
@@ -58,10 +58,10 @@ namespace BDKS_06
             return msg;
         }
 
-        public static byte[] Comm(byte b1 ,byte b2 ,ushort b3 , ushort b4 )
+        public static byte[] Comm(byte b1 ,byte b2 ,ushort s3 , ushort s4 )
         {
-            byte[] bytesOne = BitConverter.GetBytes(b3);
-            byte[] bytesTwo = BitConverter.GetBytes(b4);
+            byte[] bytesOne = BitConverter.GetBytes(s3);
+            byte[] bytesTwo = BitConverter.GetBytes(s4);
             
             byte[] msg = new byte[] {
                 b1,
@@ -79,14 +79,15 @@ namespace BDKS_06
             return msg;
         }
 
-        public static byte[] Comm(byte b1, byte b2, byte b3, ushort b4, ushort b5)
+        public static byte[] Comm(byte b1, byte b2, byte b3, ushort s4, ushort s5)
         {
-            byte[] bytesOne = BitConverter.GetBytes(b4);
-            byte[] bytesTwo = BitConverter.GetBytes(b5);
+            byte[] bytesOne = BitConverter.GetBytes(s4);
+            byte[] bytesTwo = BitConverter.GetBytes(s5);
 
             byte[] msg = new byte[] {
                 b1,
                 b2,
+                b3,
                 bytesOne[0],
                 bytesOne[1],
                 bytesTwo[0],
@@ -100,15 +101,16 @@ namespace BDKS_06
             return msg;
         }
 
-        public static byte[] Comm(byte b1, byte b2, byte b3, ushort b4, ushort b5,ushort b6)
+        public static byte[] Comm(byte b1, byte b2, byte b3, ushort s4, ushort s5,ushort s6)
         {
-            byte[] bytesOne = BitConverter.GetBytes(b4);
-            byte[] bytesTwo = BitConverter.GetBytes(b5);
-            byte[] bytesThree = BitConverter.GetBytes(b6);
+            byte[] bytesOne = BitConverter.GetBytes(s4);
+            byte[] bytesTwo = BitConverter.GetBytes(s5);
+            byte[] bytesThree = BitConverter.GetBytes(s6);
 
             byte[] msg = new byte[] {
                 b1,
                 b2,
+                b3,
                 bytesOne[0],
                 bytesOne[1],
                 bytesTwo[0],
@@ -124,16 +126,18 @@ namespace BDKS_06
             return msg;
         }
 
-        public static byte[] Comm(byte b1, byte b2, byte b3, ushort b4, ushort b5, ushort b6,ushort b7)
+
+        public static byte[] Comm(byte b1, byte b2, byte b3, ushort s4, ushort s5, ushort s6,ushort s7)
         {
-            byte[] bytesOne = BitConverter.GetBytes(b4);
-            byte[] bytesTwo = BitConverter.GetBytes(b5);
-            byte[] bytesThree = BitConverter.GetBytes(b6);
-            byte[] bytesFour = BitConverter.GetBytes(b7);
+            byte[] bytesOne = BitConverter.GetBytes(s4);
+            byte[] bytesTwo = BitConverter.GetBytes(s5);
+            byte[] bytesThree = BitConverter.GetBytes(s6);
+            byte[] bytesFour = BitConverter.GetBytes(s7);
 
             byte[] msg = new byte[] {
                 b1,
                 b2,
+                b3,
                 bytesOne[0],
                 bytesOne[1],
                 bytesTwo[0],
@@ -151,16 +155,43 @@ namespace BDKS_06
             return msg;
         }
 
-        public static byte[] Comm(byte b1, byte b2, byte b3, ushort b4, ushort b5, ushort b6,byte b7 , byte b8 , byte b9, byte b10 , byte b11 , byte b12)
+        public static byte[] Comm(byte b1, byte b2, byte b3, ushort s4, ushort s5, byte b7, byte b8, byte b9, byte b10)
         {
-            byte[] bytesOne = BitConverter.GetBytes(b4);
-            byte[] bytesTwo = BitConverter.GetBytes(b5);
-            byte[] bytesThree = BitConverter.GetBytes(b6);
-           
+            byte[] bytesOne = BitConverter.GetBytes(s4);
+            byte[] bytesTwo = BitConverter.GetBytes(s5);
 
             byte[] msg = new byte[] {
                 b1,
                 b2,
+                b3,
+                bytesOne[0],
+                bytesOne[1],
+                bytesTwo[0],
+                bytesTwo[1],
+                b7,
+                b8,
+                b9,
+                b10
+                
+            };
+
+            msg = crc.GetCRC(msg);
+            port.Write(msg);
+            msg = port.Read();
+
+            return msg;
+        }
+
+        public static byte[] Comm(byte b1, byte b2, byte b3, ushort s4, ushort s5, ushort s6,byte b7 , byte b8 , byte b9, byte b10 , byte b11 , byte b12)
+        {
+            byte[] bytesOne = BitConverter.GetBytes(s4);
+            byte[] bytesTwo = BitConverter.GetBytes(s5);
+            byte[] bytesThree = BitConverter.GetBytes(s6);          
+
+            byte[] msg = new byte[] {
+                b1,
+                b2,
+                b3,
                 bytesOne[0],
                 bytesOne[1],
                 bytesTwo[0],
